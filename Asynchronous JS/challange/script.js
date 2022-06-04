6d285fe7cd50fb6cabcd5735ea490ac0017bf977
stsp93 <stspsp@gmail.com>
2022-06-05 00:05:41 +0300

    Solvin Promise Challenge

--- Asynchronous JS/challange/script.js
+++ Asynchronous JS/challange/script.js
@@ -5,14 +5,35 @@ function createImage(imgPath) {
     return new Promise((resolve, reject) => {
         const imgEl = document.createElement('img');
         imgEl.src = imgPath;
-        imgEl.addEventListener('load', function(){
+        imgEl.addEventListener('load', function () {
             imagesEl.append(imgEl);
             resolve(imgEl)
         });
-        imgEl.addEventListener('error', function() {
+        imgEl.addEventListener('error', function () {
             reject(new Error('Image not Found'));
         })
     })
 }
+function wait(seconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, 1000 * seconds);
+    })
+}
 
-createImage('img/img-3.jpg').then(img => console.log(img)).catch(err => console.error(`Error: ${err}`));
+let curImg;
+createImage('img/img-1.jpg').then(img => {
+    curImg = img;
+    console.log('img loaded');
+    return wait(2);
+}).then(() => {
+    curImg.style.display = 'none'
+    return(createImage('img/img-2.jpg'))
+}).then(img => {
+    curImg = img;
+    console.log('img loaded');
+    return wait(2);
+}).then(() => {
+    curImg.style.display = 'none'
+    return(createImage('img/img-3.jpg'))
+})
+    .catch(err => console.error(`Error: ${err}`));
