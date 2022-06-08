67241aeab9ada0a234ae011f412d29431a77c0e0
stsp93 <stspsp@gmail.com>
2022-06-09 01:28:35 +0300

    Module Pattern

--- Js Modules and Tooling/script.js
+++ Js Modules and Tooling/script.js
@@ -3,8 +3,40 @@
 // AddToCart('bread', 3);
 // console.log(cart);
 
-import * as ShoppingCart from './shoppingCart.js';
-console.log('Module Imported');
-ShoppingCart.addToCart('bread', 3);
-console.log(ShoppingCart);
+// import * as ShoppingCart from './shoppingCart.js';
+// console.log('Module Imported');
+// ShoppingCart.addToCart('bread', 3);
+// console.log(ShoppingCart);
 
+// Module pattern
+
+const ShoppingCart2 = (function() {
+    const cart = [];
+    const totalPrice = 500;
+    const totalQuantity = 41;
+    const shippingCost = 5;
+
+    const addToCart = function(product, quantity) {
+        cart.push({product, quantity});
+        console.log(`Added ${quantity} ${product} to the cart`);
+    };
+
+    const orderProd = function(product, quantity) {
+        console.log(`Ordered ${quantity} ${product} with shipping cost of ${shippingCost}`);
+    };
+
+    return {
+        cart,
+        totalPrice,
+        totalQuantity,
+        addToCart,
+        orderProd
+    }
+})();
+
+ShoppingCart2.addToCart('milk', 2);
+ShoppingCart2.addToCart('butter', 8);
+ShoppingCart2.orderProd('tea', 10);
+console.log(ShoppingCart2.shippingCost); 
+console.log(ShoppingCart2.cart);
+console.log(ShoppingCart2);
