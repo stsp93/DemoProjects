55053ec4af44e367525680562a5990663324ca69
stsp93 <stspsp@gmail.com>
2022-06-09 15:55:26 +0300

    npm modules practice

--- Js Modules and Tooling/script.js
+++ Js Modules and Tooling/script.js
@@ -3,6 +3,7 @@
 // AddToCart('bread', 3);
 // console.log(cart);
 
+
 // import * as ShoppingCart from './shoppingCart.js';
 // console.log('Module Imported');
 // ShoppingCart.addToCart('bread', 3);
@@ -10,33 +11,55 @@
 
 // Module pattern
 
-const ShoppingCart2 = (function() {
-    const cart = [];
-    const totalPrice = 500;
-    const totalQuantity = 41;
-    const shippingCost = 5;
+// const ShoppingCart2 = (function() {
+//     const cart = [];
+//     const totalPrice = 500;
+//     const totalQuantity = 41;
+//     const shippingCost = 5;
 
-    const addToCart = function(product, quantity) {
-        cart.push({product, quantity});
-        console.log(`Added ${quantity} ${product} to the cart`);
-    };
+//     const addToCart = function(product, quantity) {
+//         cart.push({product, quantity});
+//         console.log(`Added ${quantity} ${product} to the cart`);
+//     };
 
-    const orderProd = function(product, quantity) {
-        console.log(`Ordered ${quantity} ${product} with shipping cost of ${shippingCost}`);
-    };
+//     const orderProd = function(product, quantity) {
+//         console.log(`Ordered ${quantity} ${product} with shipping cost of ${shippingCost}`);
+//     };
 
-    return {
-        cart,
-        totalPrice,
-        totalQuantity,
-        addToCart,
-        orderProd
-    }
-})();
+//     return {
+//         cart,
+//         totalPrice,
+//         totalQuantity,
+//         addToCart,
+//         orderProd
+//     }
+// })();
 
-ShoppingCart2.addToCart('milk', 2);
-ShoppingCart2.addToCart('butter', 8);
-ShoppingCart2.orderProd('tea', 10);
-console.log(ShoppingCart2.shippingCost); 
-console.log(ShoppingCart2.cart);
-console.log(ShoppingCart2);
+// ShoppingCart2.addToCart('milk', 2);
+// ShoppingCart2.addToCart('butter', 8);
+// ShoppingCart2.orderProd('tea', 10);
+// console.log(ShoppingCart2.shippingCost); 
+// console.log(ShoppingCart2.cart);
+// console.log(ShoppingCart2);
+
+
+// Practicing npm
+
+import  cloneDeep  from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+    cart: [
+        {product: 'pizza', quantity: 10,},
+        {product: 'lasagne', quantity: 8,}
+    ],
+    user: {loggedIn: true},
+}
+
+// const stateClone = Object.assign({}, state)
+//     state.user.loggedIn = false;
+// console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateDeepClone);
+console.log(state);
