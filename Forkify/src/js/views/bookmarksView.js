0b6ac622b6beda7a6eb256be38a44bb0406cbddb
stsp93 <stspsp@gmail.com>
2022-07-21 10:10:06 +0300

    refactor bookmarks

---
+++ Forkify/src/js/views/bookmarksView.js
@@ -0,0 +1,15 @@
+import previewView from './previewView';
+import View from "./View";
+
+class bookmarksView extends View {
+    _parentElement = document.querySelector('.bookmarks__list');
+    _errorMessage = 'No bookmarks yet!';
+    _message = '';
+
+    _generateMarkup() {
+        return this._data.map(bookmark => previewView.render(bookmark, false)).join('')
+    };
+
+}
+
+export default new bookmarksView();
